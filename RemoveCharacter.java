c84deeb53f7414b3b2cfad9503edc15a9553de7d
berkekocar <e.berkekocar@gmail.com>
2020-09-28 10:27:57 +0100

    Added user input

--- RemoveCharacter.java
+++ RemoveCharacter.java
@@ -2,20 +2,25 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class RemoveCharacter {
  
     public static void main(String[] args) {
         try {
+
             FileReader reader = new FileReader("README.txt");
             BufferedReader bufferedReader = new BufferedReader(reader);
             FileWriter writer = new FileWriter("MyFile.txt", true);
-
+            Scanner sc = new Scanner(System.in);
             String line;
+            
+            System.out.println("Which character would you like to remove?");
+            String ch = sc.nextLine();
  
             while ((line = bufferedReader.readLine()) != null) {
                 
-                writer.write(line=line.replace("*",""));
+                writer.write(line=line.replace(ch,""));
                 writer.write("\r\n");
 
                 System.out.println(line);
